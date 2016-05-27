a99e6d73621cab0266d72aaebb98fd1443c00549
krispassa <krispassa@gmail.com>
2016-05-26 18:52:40 -0700

    added menu list

--- Main.js
+++ Main.js
@@ -1,1 +1,13 @@
 var Observable = require('FuseJS/Observable');
+var menuList = Observable();
+menuList.add({menuName:"Activity"});
+menuList.add({menuName:"Members"});
+menuList.add({menuName:"Events"});
+menuList.add({menuName:"YouTube"});
+menuList.add({menuName:"Office"});
+menuList.add({menuName:"Admin"});
+
+
+module.exports = {
+	menuList: menuList
+}
