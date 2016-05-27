58bbf1fee4725ecc1d40f002068e9394434ec21d
krispassa <krispassa@gmail.com>
2016-05-27 12:00:12 -0700

    fix padding and adding .ux file

--- Main.js
+++ Main.js
@@ -3,7 +3,6 @@ var menuList = Observable();
 menuList.add({menuName:"Activity"});
 menuList.add({menuName:"Members"});
 menuList.add({menuName:"Events"});
-menuList.add({menuName:"YouTube"});
 menuList.add({menuName:"Office"});
 menuList.add({menuName:"Admin"});
 
